f90aadba9f748349c7acea084837e21248100c95
Matthieu Nantern <matthieu@nantern.com>
2018-08-09 19:05:49 +0200

    add git aliases (fork and uncommit) + fix issue with brew install (Brewfile was not used if brew was already installed)

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -5,6 +5,8 @@
 # This installs some of the common dependencies needed (or at least desired)
 # using Homebrew.
 
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 # Check for Homebrew
 if test ! $(which brew)
 then
@@ -13,17 +15,17 @@ then
   # Install the correct homebrew for each OS type
   if test "$(uname)" = "Darwin"
   then
-    SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-    brew update
-    brew tap caskroom/cask
-    brew tap Homebrew/bundle
-    brew bundle --file=${SCRIPT_DIR}/Brewfile
   elif test "$(expr substr $(uname -s) 1 5)" = "Linux"
   then
     echo "On linux don't need Homebrew"
+    exit 1
   fi
-
 fi
+
+brew update
+brew tap caskroom/cask
+brew tap Homebrew/bundle
+brew bundle --file=${SCRIPT_DIR}/Brewfile
 
 exit 0
