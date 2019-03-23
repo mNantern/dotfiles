0d760cec09c47fbab346116eb57c2e4d1f333c87
Matthieu Nantern <matthieu@nantern.com>
2019-03-23 14:43:56 +0100

    Install liquidprompt

--- liquidprompt/install.sh
+++ liquidprompt/install.sh
@@ -3,3 +3,12 @@
 # Liquidprompt
 #
 # Install Liquidprompt: https://github.com/nojhan/liquidprompt
+
+mkdir -p ~/Projects/liquidprompt
+cd ~/Projects/liquidprompt/
+curl -LO https://github.com/nojhan/liquidprompt/archive/master.zip
+unzip master.zip
+rm master.zip
+
+mkdir -p ~/.bashrc.d/
+echo "[[ \$- = *i* ]] && source ~/Projects/liquidprompt/liquidprompt" > ~/.bashrc.d/liquidprompt.bashrc
